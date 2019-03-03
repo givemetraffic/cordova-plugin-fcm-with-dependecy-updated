ac73a7bfa14110cf35c44b4179098b7fa51c2ab6
Igor Nikitin <igor.v.nikitin@gmail.com>
2019-03-03 20:27:58 +0300

    Hotfix

--- src/ios/AppDelegate+FCMPlugin.h
+++ src/ios/AppDelegate+FCMPlugin.h
@@ -12,25 +12,22 @@
 
 #import "Firebase.h"
 
-#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
-@import UserNotifications;
-#endif
-
-// Implement UNUserNotificationCenterDelegate to receive display notification via APNS for devices
-// running iOS 10 and above. Implement FIRMessagingDelegate to receive data message via FCM for
-// devices running iOS 10 and above.
-#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
-@interface AppDelegate () <UNUserNotificationCenterDelegate, FIRMessagingDelegate>
-@end
-#endif
-
-// Copied from Apple's header in case it is missing in some cases (e.g. pre-Xcode 8 builds).
 #ifndef NSFoundationVersionNumber_iOS_9_x_Max
 #define NSFoundationVersionNumber_iOS_9_x_Max 1299
 #endif
 
-@interface AppDelegate (FCMPlugin)
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+@import UserNotifications;
+
+@interface AppDelegate (FCMPlugin) <UNUserNotificationCenterDelegate, FIRMessagingDelegate>
+
+@end
+#else
+
+@interface AppDelegate (FCMPlugin) <FIRMessagingDelegate>
 
 + (NSData*)getLastPush;
 
 @end
+#endif
+
